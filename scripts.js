87f8a162acccb82b19023f1d254285787cb0afe3
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-02-17 23:54:50 -0300

    Add function DOM

--- scripts.js
+++ scripts.js
@@ -17,3 +17,90 @@ const Modal = {
         .remove('active')
     }
   }
+
+  const transactions = [
+    {
+      id: 1,
+      description: 'Luz',
+      amount: -50000,
+      date: '16/02/2021'
+    },
+    {
+      id: 2,
+      description: 'Internet',
+      amount: 500000,
+      date: '16/02/2021'
+    },
+    {
+      id: 3,
+      description: 'Água',
+      amount: -20000,
+      date: '16/02/2021'
+    },
+    {
+      id: 4,
+      description: 'Site',
+      amount: 2000000,
+      date: '16/02/2021'
+    }
+  ]
+
+  // 1° Somar as entradas
+  // 2° Somar as saídas
+  // 3° Subtrair das entradas o valor das saídas
+  // 4° Atribuir o total
+  const Transaction = {
+    incomes() {  
+      // Somar as entradas
+    },
+
+    expenses() {
+      // Somar as saídas
+    },
+
+    total() {
+      // incomes - expenses
+    }
+  }
+
+  // Substituir os dados do HTML com os dados do JS.
+
+  const DOM = {
+    transactionContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+      const tr = document.createElement('tr')
+      tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+      DOM.transactionContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction) {
+
+
+
+      const html = `
+      <td class="description"> 
+        ${transaction.description}
+      </td>
+      <td class="expense">
+        ${transaction.amount}
+      </td>
+      <td class="date">
+        ${transaction.date}
+      </td>
+      <td>
+        <img
+          src="./assets/minus.svg"
+          alt="Remover Transação"
+        >
+      </td>
+      `
+
+      return html
+    }
+  }
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transaction)
+})
