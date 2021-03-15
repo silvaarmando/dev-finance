80cbd7f3f3bd68c47acd46ddf8d70033daaaf7fc
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-03-15 20:12:57 -0300

    Add favicon

--- scripts.js
+++ scripts.js
@@ -1,3 +1,42 @@
+const root = document.querySelector(':root')
+const checkbox =document.querySelector('input[name=theme]')
+
+const getStyle = (element, style) =>
+  window
+    .getComputedStyle(element)
+    .getPropertyValue(style)
+
+const initialColors = {
+  darkBlue: getStyle(root, "--dark-blue"),
+  bgWhite: getStyle(root, "--bg-white"),
+  white: getStyle(root, "--white")
+}
+
+const darkMode = {
+  darkBlue: "#ffffff",
+  bgWhite: "#313131",
+  white: "#808080"
+}
+
+const transformKey = key =>
+  "--" + key
+    .replace(/([A-Z])/, "-$1")
+    .toLowerCase()
+
+const changeColors = (colors) => {
+  Object
+    .keys(colors)
+    .map(key =>
+      root
+        .style
+        .setProperty(transformKey(key), colors[key])
+  )
+}
+
+checkbox.addEventListener('change', ({target}) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
+
 const Modal = {
   open() {
     document
